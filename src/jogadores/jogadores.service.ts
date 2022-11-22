9bc35e762f95924aaa5d62a37ce209ec006712a9
Helder Santos <helders@motorola.com>
2022-11-22 00:49:20 -0400

    consultando e removendo jogador
    Signed-off-by: Helder Santos <helders@motorola.com>

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
@@ -8,12 +8,33 @@ export class JogadoresService {
   private readonly logger = new Logger(JogadoresService.name);
   private jogadores: Jogador[] = [];
 
-  async criarAtualizarJogador(
-    criarJogadorDto: CriarJogadorDto,
-  ): Promise<Jogador[]> {
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
     this.logger.log(`criaJogadorDto: ${JSON.stringify(criarJogadorDto)}`);
-    this.criar(criarJogadorDto);
+
+    const { email } = criarJogadorDto;
+
+    const jogadorEncontrado = this.jogadores.find((jogador) => jogador.email === email);
+
+    if (jogadorEncontrado) {
+      this.atualizar(jogadorEncontrado, criarJogadorDto);
+    } else {
+      this.criar(criarJogadorDto);
+    }
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
     return this.jogadores;
+  }
+
+  async consultarJogadoresPeloEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = this.jogadores.find((jogador) => jogador.email === email);
+    if (jogadorEncontrado) return jogadorEncontrado;
+
+    throw new NotFoundException(`Jogador com email: ${email} não encontrado!`);
+  }
+
+  async deletarJogadorPeloEmail(email: string): Promise<void> {
+    this.jogadores = this.jogadores.filter((jogador) => jogador.email !== email);
   }
 
   private criar(criaJogadorDto: CriarJogadorDto): void {
@@ -30,5 +51,10 @@ export class JogadoresService {
     };
 
     this.jogadores.push(jogador);
+  }
+
+  private atualizar(jogadorEncontrado: Jogador, criarJogadorDto: CriarJogadorDto): void {
+    const { nome } = criarJogadorDto;
+    jogadorEncontrado.nome = nome;
   }
 }
