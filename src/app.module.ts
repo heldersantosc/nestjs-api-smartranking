c34babed85863d0fcdcff64070ca08e6718034bb
Helder Santos <helders@motorola.com>
2022-12-11 23:23:06 -0400

    Criado novo modulo categorias
    Signed-off-by: Helder Santos <helders@motorola.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import * as dotenv from 'dotenv';
 import { JogadoresModule } from './jogadores/module/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), JogadoresModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), JogadoresModule, CategoriasModule],
   controllers: [],
   providers: [],
 })
